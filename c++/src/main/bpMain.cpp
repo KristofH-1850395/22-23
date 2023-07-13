18441c8573b5722d8991e5d88f431773fded0b97
KristofH-1850395 <kristof.heyndels@student.uhasselt.be>
2023-07-13 08:22:31 +0200

    some updates to main functions

--- c++/src/main/bpMain.cpp
+++ c++/src/main/bpMain.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
@@ -13,7 +14,7 @@ int main(int argc, char *argv[]) {
     std::cout << "executing program" << std::endl;
     
     // int simtime = the first argument
-    int simTime = std::atoi(argv[1]);
+    int simulationTime = std::atoi(argv[1]);
 
     // float infectionRate = the second argument
     float infectionRate = std::atof(argv[2]);
@@ -24,6 +25,12 @@ int main(int argc, char *argv[]) {
     // string outputPath = the fourth argument
     std::string outputPath = argv[4];
 
+    // write metadata to meta.txt
+    std::ofstream metadata(outputPath + "metadata.txt");
+    std::string metadataString = "simulation time: " + std::to_string(simulationTime) + "\n";
+    metadataString += "ensemble size: " + std::to_string(ensembleSize) + "\n";    
+    metadata << metadataString;
+
     // run the simulation for infectionRate +- a delta
     double delta = 0.00128;
     int range = 10;
@@ -32,7 +39,7 @@ int main(int argc, char *argv[]) {
 
     for (double lambda = infectionRate - (delta * range); lambda <= infectionRate + (delta * range); lambda += delta) {
        std::cout << "Iteration " << iteration << "/" << 2*range+1 << std::endl;
-        Application::simulateBachelorProcess(simTime, ensembleSize, lambda, 1000, outputPath);
+        Application::simulateBachelorProcess(simulationTime, ensembleSize, lambda, 1000, outputPath);
 
         iteration++;
     }
--- c++/src/main/bpMainFinite.cpp
+++ c++/src/main/bpMainFinite.cpp
@@ -1,12 +1,9 @@
 #include "../../include/application.h"
 
-#include <fstream>
-#include <iostream>
-#include <stdlib.h>
 #include <string>
-#include <vector>
+#include <iostream>
+#include <fstream>
 #include <cmath>
-#include <omp.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
@@ -18,7 +15,7 @@ int main(int argc, char *argv[]) {
     std::cout << "executing program" << std::endl;
     
     // int simtime = the first argument
-    int simTime = std::atoi(argv[1]);
+    int simulationTime = std::atoi(argv[1]);
 
     // float infectionRate = the second argument
     float infectionRate = std::atof(argv[2]);
@@ -29,6 +26,12 @@ int main(int argc, char *argv[]) {
     // string outputPath = the fourth argument
     std::string outputPath = argv[4];
 
+    // write metadata to meta.txt
+    std::ofstream metadata(outputPath + "metadata.txt");
+    std::string metadataString = "simulation time: " + std::to_string(simulationTime) + "\n";
+    metadataString += "ensemble size: " + std::to_string(ensembleSize) + "\n";    
+    metadata << metadataString;
+
     // run the simulation for infectionRate +- a delta
     double delta = 0.0128;
     int range = 10;
@@ -37,7 +40,7 @@ int main(int argc, char *argv[]) {
 
     for (int systemSize = 8; systemSize <= pow(2,12); systemSize *= 2) {
         std::cout << "Iteration " << iteration << std::endl;
-        Application::simulateBachelorProcess(simTime, ensembleSize, infectionRate, systemSize, outputPath);
+        Application::simulateBachelorProcess(simulationTime, ensembleSize, infectionRate, systemSize, outputPath);
 
         iteration++;
     }
