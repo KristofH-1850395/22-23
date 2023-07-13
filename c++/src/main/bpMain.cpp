2e57304473f5adf95a67544ebe26f4cf9fec8518
KristofH-1850395 <kristof.heyndels@student.uhasselt.be>
2023-07-13 08:06:45 +0200

    added iteration max count

--- c++/src/main/bpMain.cpp
+++ c++/src/main/bpMain.cpp
@@ -25,13 +25,13 @@ int main(int argc, char *argv[]) {
     std::string outputPath = argv[4];
 
     // run the simulation for infectionRate +- a delta
-    double delta = 0.1;
-    int range = 5;
+    double delta = 0.00128;
+    int range = 10;
 
     int iteration = 1;
 
     for (double lambda = infectionRate - (delta * range); lambda <= infectionRate + (delta * range); lambda += delta) {
-        std::cout << "Iteration " << iteration << std::endl;
+       std::cout << "Iteration " << iteration << "/" << 2*range+1 << std::endl;
         Application::simulateBachelorProcess(simTime, ensembleSize, lambda, 1000, outputPath);
 
         iteration++;
